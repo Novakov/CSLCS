60c295d2d6154ee9f201e7c2211dc35fe1260ae8
Kamil Maciejczek <maciejczek.kamil@gmail.com>
2014-12-16 20:03:15 +0100

    Added StatCRect unit tests

---
+++ Tests/StatCRectTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSL.Statistics;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class StatCRectTests
+    {
+        [Test]
+        public void ShouldCalculateProperAverageAndDeviation()
+        {
+            // arrange
+            var stat = new StatCRect();
+            stat.Add(1, 0);
+            stat.Add(0, 1);
+            stat.Add(1, 2);
+            stat.Add(0, 3);
+            stat.Add(1, 4);
+
+            // act
+            double avg = 0;
+            double dev = 0;
+            stat.GetStat(ref avg, ref dev);
+            double avg2 = 0;
+            stat.GetStat(ref avg2);
+            double avg3 = 0;
+            double dev3 = 0;
+            stat.GetStat(ref avg3, ref dev3, 4); 
+            double avg4 = 0;
+            stat.GetStat(ref avg4);
+
+            // assert
+            Assert.That(avg, Is.EqualTo(0.5));
+            Assert.That(dev, Is.EqualTo(0.44721).Within(0.00001));
+
+            Assert.That(avg2, Is.EqualTo(0.5));
+
+            Assert.That(avg3, Is.EqualTo(0.5));
+            Assert.That(dev3, Is.EqualTo(0.5).Within(0.00001));
+
+            Assert.That(avg4, Is.EqualTo(0.5));
+        }
+
+        [Test]
+        public void ClearingShouldEmptyStat()
+        {
+            // arrange
+            var stat = new StatCRect();
+
+            stat.Add(1, 0);
+            stat.Add(0, 1);
+            stat.Add(1, 2);
+            stat.Add(0, 3);
+            stat.Add(1, 4);
+
+            // act
+            stat.Clear();
+
+            // assert
+            double x = 0;
+            Assert.That(() => stat.GetStat(ref x), Throws.TypeOf<ArgumentOutOfRangeException>());
+        }
+    }
+}
